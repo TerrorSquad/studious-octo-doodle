6a1f1ab1bc5a4d33e6ec941c7a14493bcee57165
Goran Ninkovic <g.ninkovic@beeit.rs>
2021-06-03 22:40:53 +0200

    feat: implement the desired functionality
    command will now generate CSV and output it to STDOUT

--- cmd/root.go
+++ cmd/root.go
@@ -16,29 +16,106 @@ limitations under the License.
 package cmd
 
 import (
+	"bytes"
+	"encoding/csv"
+	"errors"
 	"fmt"
-	"os"
-	"github.com/spf13/cobra"
-
 	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"io/ioutil"
+	"log"
+	"os"
+	"regexp"
 )
+
+type Product struct {
+	sku    string
+	images map[string]string
+}
 
 var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "bicsv",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "Generates a CSV files that contains data from bulk image import",
+	Long: `The command accepts exactly one argument, path to the images directory.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+It will generate a CSV string and output it to STDOUT when called.
+CSV will have the following format:
+sku, base_image, small_image, thumbnail_image, rollover_image
+
+Example: bicsv ./product_images
+`,
+	Args: cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		imagePath := args[0]
+
+		files, err := ioutil.ReadDir(imagePath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		re := regexp.MustCompile(`(?P<sku>\d+)_(?P<suffix>\d)(?P<extension>\.jpg|png)`)
+		var products = map[string]Product{}
+		for _, file := range files {
+			var match = re.FindAllStringSubmatch(file.Name(), -1)
+			if len(match) > 0 {
+				sku := match[0][1]
+				suffix := match[0][2]
+				if products[sku].sku == "" {
+					products[sku] = updateImagesMapForProduct(Product{
+						sku:    sku,
+						images: map[string]string{},
+					}, suffix, file.Name())
+				} else {
+					if sku == products[sku].sku {
+						updateImagesMapForProduct(products[sku], suffix, file.Name())
+					}
+				}
+			}
+		}
+		headers := []string{"sku", "base_image", "small_image", "thumbnail_image", "rollover_image"}
+		var rows [][]string
+		for _, product := range products {
+			rows = append(rows, []string{
+				product.sku,
+				product.images["baseImage"],
+				product.images["smallImage"],
+				product.images["thumbnailImage"],
+				product.images["rolloverImage"]})
+		}
+		byteData, err := WriteAll(append([][]string{headers}, rows...))
+		fmt.Print(string(byteData))
+	},
+}
+
+func updateImagesMapForProduct(product Product, suffix string, fileName string) Product {
+	if suffix == "1" {
+		product.images["baseImage"] = fileName
+		product.images["smallImage"] = fileName
+		product.images["thumbnailImage"] = fileName
+	} else if suffix == "2" {
+		product.images["rolloverImage"] = fileName
+	}
+	return product
+}
+
+func WriteAll(records [][]string) ([]byte, error) {
+	if records == nil || len(records) == 0 {
+		return nil, errors.New("records cannot be nil or empty")
+	}
+	var buf bytes.Buffer
+	csvWriter := csv.NewWriter(&buf)
+	err := csvWriter.WriteAll(records)
+	if err != nil {
+		return nil, err
+	}
+	csvWriter.Flush()
+	if err := csvWriter.Error(); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
